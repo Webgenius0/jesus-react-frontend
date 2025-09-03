b6c29cad834c8a161592aed3f2eb1aa6e8844e92
Johirul <jahirulislamrayhan07@gmail.com>
2025-09-03 09:52:19 +0600

    testing

---
+++ src/Pages/FranchiseeDashboardPages/AllProducts.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AllProducts() {
+  return (
+    <main>
+      <div className="flex gap-6">
+        <div className="">Catagory</div>
+        <div className="">Product</div>
+      </div>
+    </main>
+  );
+}
--- src/Pages/FranchiseeDashboardPages/FranchiseeOverview.jsx
+++ src/Pages/FranchiseeDashboardPages/FranchiseeOverview.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { DollarSign, Percent, Users, Star } from "lucide-react";
 import FranchiseeOverviewCharts from "@/Components/FranchiseeOverviewPages/FranchiseeOverviewCharts";
-import Component from "@/Components/FranchiseeOverviewPages/SalesDataUploadTable";
+import SalesDataUploadTable from "@/Components/FranchiseeOverviewPages/SalesDataUploadTable";
 
 const dashboardMetrics = [
   { id: 1, value: "$2165", label: "Today's Sale", Icon: DollarSign },
@@ -30,7 +30,7 @@ export default function FranchiseeOverview() {
         ))}
       </div>
       <FranchiseeOverviewCharts />
-      <Component/>
+      <SalesDataUploadTable/>
     </main>
   );
 }
